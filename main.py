cc1f7a106d8aa97a2865a7c11e2d143fa04d7578
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-12 13:49:15 +0000

    created 321_ComboMenuLists_Step19_Gombos.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ Max Gombos
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
 
 #exec(open("step5.py").read())
-exec(open("ComboMenu.py").read())
+#exec(open("ComboMenu.py").read())
+exec(open("321_ComboMenuLists_Step19_Gombos.py").read())
